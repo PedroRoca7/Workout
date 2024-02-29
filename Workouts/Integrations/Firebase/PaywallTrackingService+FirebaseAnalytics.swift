28452db8ae2bc39be87cbdd208a6067b111fb363
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2024-02-29 15:30:33 -0300

    commit

--- Workouts/Integrations/Firebase/PaywallTrackingService+FirebaseAnalytics.swift
+++ Workouts/Integrations/Firebase/PaywallTrackingService+FirebaseAnalytics.swift
@@ -4,14 +4,14 @@ import WorkoutsCore
 import Payment
 
 struct FirebaseAnalyticsPaywallTrackingService: PaywallTrackingService {
-    func trackView(workout: WorkoutsCore.Workout?) {
+    func trackView(workout: Workout?) {
         Analytics.logEvent(
             "Hit Paywall",
             parameters: ["workoutID": workout?.id ?? "no workout"]
         )
     }
     
-    func trackPurchase(workout: WorkoutsCore.Workout?) {
+    func trackPurchase(workout: Workout?) {
         Analytics.logEvent(
             "New Subscription",
             parameters: ["workoutID": workout?.id ?? "no workout"]
